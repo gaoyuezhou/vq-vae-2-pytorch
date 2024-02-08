2601af56d791db02220ae73443cc22d40c4833a3
gaoyuezhou <zhougy_99@berkeley.edu>
2024-02-08 16:59:37 -0500

    epic dataset using videoloader

--- epic_dataset.py
+++ epic_dataset.py
@@ -9,8 +9,92 @@ import random
 from tqdm import tqdm
 import einops
 import decord
-from decord import VideoReader
+from decord import VideoReader, VideoLoader
+from decord import cpu, gpu
 decord.bridge.set_bridge("torch")
+
+class EpicKitchenVideoDatasetV2(Dataset):
+    def __init__(
+        self,
+        folder='/vast/gz2123/datasets/torrent/3h91syskeag572hl6tvuovwv4d/videos',
+        transform=None,
+        img_size=[270, 480],
+        frameskip=1,
+        window_size=1,
+        mode="train",
+        metadata="metadata.json",
+        as_image=False,
+    ) -> None:
+        
+        self.folder = folder
+        self.frameskip = frameskip
+        self.window_size = window_size
+        self.transform = transform
+        self.as_image = as_image
+        if not os.path.exists(os.path.join(folder, "metadata.json")):
+            self.gen_metadata()
+        with open(os.path.join(self.folder, "metadata.json")) as json_file:
+            metadata = json.load(json_file)
+        self.metadata = metadata
+ 
+        num_videos = 0
+        self.video_paths = []
+        print("### Reading metadata...")
+        for key in sorted(metadata.keys()):
+            if mode in key:
+                num_videos += 1
+                self.video_paths.append(os.path.join(self.folder, key))
+        
+        video_loader = VideoLoader(
+            self.video_paths,
+            ctx=cpu(0),
+            shape=[window_size] + img_size + [3],
+            interval=1, skip=frameskip, shuffle=0
+        )
+
+        print("### FINISHED reading metadata")
+        self.size = len(video_loader)
+        self.video_loader = video_loader
+        # import pdb; pdb.set_trace()
+    
+    def __len__(self):
+        return self.size
+
+    def gen_metadata(self):
+        video_metadata = {}
+        folder_path = self.folder
+        for subfolder in os.listdir(folder_path):
+            subfolder_path = os.path.join(folder_path, subfolder) # train/test
+            for videofolder in os.listdir(subfolder_path):
+                videofolder_path = os.path.join(subfolder_path, videofolder)
+                print(videofolder_path)
+                for video_file in os.listdir(videofolder_path):
+                    print(video_file)
+                    video_path = os.path.join(videofolder_path, video_file)
+                    reader = VideoReader(
+                        video_path,
+                        num_threads=1,
+                    )
+                    frames_count = len(reader)
+                    video_metadata[f'{subfolder}/{videofolder}/{video_file}'] = frames_count
+
+        with open(os.path.join(self.folder, "metadata.json"), 'w') as json_file:
+            json.dump(video_metadata, json_file, indent=4)
+
+        
+    def __getitem__(self, idx): 
+        # print(f"### get item {idx}")
+        frames = self.video_loader.next()[0]
+        frames = einops.rearrange(frames, "T H W C -> T C H W")
+        # if self.transform:
+        #     frames = self.transform(frames.float())
+        if self.as_image:
+            frames = frames[0]
+        return frames, torch.tensor(0)
+    
+    def __len__(self):
+        return self.size
+
 
 class EpicKitchenVideoDataset(Dataset): 
     def __init__(
@@ -76,7 +160,7 @@ class EpicKitchenVideoDataset(Dataset):
 
         
     def __getitem__(self, idx): 
-        # print(f"### get item {idx}")
+        print(f"### get item {idx}")
         video_idx = bisect.bisect_right(self.cumulative_sizes, idx)
         if video_idx == 0:
             sample_idx = idx
@@ -86,18 +170,19 @@ class EpicKitchenVideoDataset(Dataset):
         reader = VideoReader(
                         video_path,
                         num_threads=1,
+                        # width=224,
+                        # height=224,
                     )
         frames = reader.get_batch(range(sample_idx, sample_idx + self.frameskip * self.window_size, self.frameskip))
         frames = einops.rearrange(frames, "T H W C -> T C H W")
+        import pdb; pdb.set_trace()
         if self.transform:
             frames = self.transform(frames.float())
         if self.as_image:
             frames = frames[0]
-        # print(f"###  item {idx} returned, shape: ", frames.shape)
+        print(f"###  item {idx} returned, shape: ", frames.shape)
         return frames, torch.tensor(0)
-    
-    def __len__(self):
-        return self.size
+
 
 if __name__ == "__main__":
     from torchvision import datasets, transforms, utils
@@ -114,9 +199,25 @@ if __name__ == "__main__":
             transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]), # not normalize at dataloader for memory consideration
         ]
     )
-    dset = EpicKitchenVideoDataset(transform=transform, frameskip=5, window_size=7, mode="train")
-    dataloader = DataLoader(dset, batch_size=3, shuffle=True, num_workers=0)
-    for i, data in enumerate(dataloader):
-        print(data.shape)
-        data = transform2(data.float())
+    # dset = EpicKitchenVideoDatasetV2(
+    #     folder="/home/kathy/Downloads/videos", 
+    #     # img_size=224, 
+    #     transform=transform2, 
+    #     frameskip=5, 
+    #     window_size=7, 
+    #     mode="train"
+    # )
+
+
+    dset = EpicKitchenVideoDataset(folder="/home/kathy/Downloads/videos", transform=transform, frameskip=5, window_size=7, mode="train")
+    dataloader = DataLoader(dset, batch_size=128, shuffle=False, num_workers=0)
+    # time the dataloader
+    import time
+    cur_time = time.time()
+    for i, (data, _) in enumerate(dataloader):
+        b = data.shape[0]
+        data = transform2(einops.rearrange(data.float(), "B T C H W -> (B T) C H W"))
+        data = einops.rearrange(data, "(B T) C H W -> B T C H W", B=b)
+        print(data.shape, time.time() - cur_time)
+
         import pdb; pdb.set_trace()
